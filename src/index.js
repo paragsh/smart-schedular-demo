025bb86ac5c60093bea1a9d1f91f0b06b5c4d160
parag <parags18@gmail.com>
2020-07-25 01:21:57 +0530

    Removed Unwanted Files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Basic from './Basic';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Basic />, document.getElementById('root'));
-registerServiceWorker();
