93f94a6bc3b10000feac267eb7bf9f483ed198c5
parag <parags18@gmail.com>
2020-07-31 02:01:15 +0530

    Updated new event layout

--- client/src/component/Scheduler/PopOver/NewEventPopOver.js
+++ client/src/component/Scheduler/PopOver/NewEventPopOver.js
@@ -1,12 +1,12 @@
-import {DARK_RED} from "../../../Constant/color";
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
 import TextField from "@material-ui/core/TextField";
-import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import React, {useState} from "react";
-import SaveIcon from '@material-ui/icons/Save';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import 'date-fns';
+import { InputLabel } from '@material-ui/core';
+import {allStaffList} from "../../../utils/getAllStaffList";
 
 export const newEventPopover = (eventItem, title, start, end, customerList, serviceList) => {
 
@@ -14,88 +14,135 @@ export const newEventPopover = (eventItem, title, start, end, customerList, serv
         alert(`You just clicked saveButtonClicked button. event title: ${eventItem.title}`);
     };
 
+    const handleDateChange = (eventItem) => {
+        // alert(`You just clicked handleDateChange button. event title: ${eventItem.title}`);
+    };
 
+
+    const duration= 60;
+    const endTime = start.add(duration, 'minutes');
     return (
         <div style={{width: '600px'}}>
-            <Row type="flex" align="middle" style={{width: '600px', height: '40px'}}>
-                <Col span={2}>
-                    {/*<div className="status-dot" style={{backgroundColor: statusColor}} />*/}
-                </Col>
+            <Row type="flex" align="middle" style={{height:40}}>
+                <Col span={2}/>
                 <Col span={22} className="overflow-text">
-                    <span className="header2-text" title={eventItem.title}/>
-                    <span className="header2-text" title={title}>{title}</span>
+                    <span className="header2-text" title={title}>New Appointment</span>
                 </Col>
             </Row>
             <form noValidate autoComplete="off">
-                <Row type="flex" align="middle">
+                <Row type="flex" align="middle" style={{ marginTop:5,marginDown:5}}>
                     <Col span={2}/>
                     <Col span={22} className="overflow-text">
-                        <div>
-                            <Autocomplete
-                                id="combo-box-demo"
-                                options={customerList}
-                                getOptionLabel={(option) => option['customer_Name']}
-                                style={{ width: 300 }}
-                                size="small"
-                                renderInput={(params) => <TextField {...params} margin="normal" label="Customer Name" variant="outlined" />}
-                            />
-                        </div>
-                    </Col>
-                </Row>
-                <Row type="flex" align="middle">
-                    <Col span={2}/>
-                    <Col span={22} className="overflow-text">
-                        <div>
-                            <Autocomplete
-                                id="combo-box-demo"
-                                options={serviceList}
-                                size="small"
-                                getOptionLabel={(option) => option['name']}
-                                style={{ width: 300 }}
-                                renderInput={(params) => <TextField {...params}  margin="normal"  label="Service" variant="outlined" />}
-                            />
-                        </div>
-                    </Col>
-                </Row>
-                <Row type="flex" align="middle" style={{paddingTop: 10}}>
-                    <Col span={2}/>
-                    <Col span={22} className="overflow-text">
-                        <div>
-                            <span className="header2-text">Duration : </span>
-                            <span className="header1-text">45 mins</span>
-                        </div>
+                        <TextField
+                            id="selectDate"
+                            label="Select Date"
+                            type="date"
+                            onChange={handleDateChange}
+                            variant="outlined"
+                            size="small"
+                            margin="normal"
+                            defaultValue={start.format("YYYY-MM-DD")}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
                     </Col>
                 </Row>
 
+                <Row type="flex" align="middle" style={{ marginTop:5,marginDown:5}}>
+                    <Col span={2}/>
+                    <Col span={4} className="overflow-text">
+                        <TextField
+                            disabled
+                            id="Duration"
+                            label="Duration"
+                            defaultValue={duration +' mins'}
+                            variant="filled"
+                        />
+                        <InputLabel/>
+                    </Col>
+                    <Col span={2}/>
+                    <Col span={6} className="overflow-text">
+                        <TextField
+                            id="StartTime"
+                            label="Start Time"
+                            onChange={handleDateChange}
+                            variant="outlined"
+                            type="time"
+                            size="small"
+                            margin="normal"
+                            defaultValue={start.format("HH:mm")}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            inputProps={{
+                                step: 1800, // 30 min
+                            }}
+                        />
+                    </Col>
+                    <Col span={2}/>
+                    <Col span={4} className="overflow-text">
+                        <TextField
+                            disabled
+                            id="End Time"
+                            label="End Time"
+                            defaultValue={endTime.format("HH:mm")}
+                            variant="filled"
+                        />
+                    </Col>
+                    <Col span={2}/>
+                </Row>
+                <Row type="flex" align="middle">
+                    <Col span={2}/>
+                    <Col span={12} className="overflow-text">
+                        <Autocomplete
+                            id="serviceList-autocomplete"
+                            options={serviceList}
+                            size="small"
+                            getOptionLabel={(option) => option['name']}
+                            renderInput={(params) => <TextField {...params}  margin="normal"  label="Service" variant="outlined" />}
+                        />
+                    </Col>
+                    <Col span={1}/>
+                    <Col span={8} className="overflow-text">
+                        <Autocomplete
+                            id="serviceList-autocomplete"
+                            options={allStaffList}
+                            size="small"
+                            getOptionLabel={(option) => option['Name']}
+                            renderInput={(params) => <TextField {...params}  margin="normal"  label="Staff" variant="outlined" />}
+                        />
+                    </Col>
+                </Row>
+                <Row type="flex" align="middle">
+                    <Col span={2}/>
+                    <Col span={12} className="overflow-text">
+                        <Autocomplete
+                            id="customerList-autocomplete"
+                            options={customerList}
+                            getOptionLabel={(option) => option['customer_Name']}
+                            size="small"
+                            renderInput={(params) => <TextField {...params} margin="normal" label="Customer Name" variant="outlined" />}
+                        />
+                    </Col>
+                </Row>
             </form>
 
-            <Row type="flex" align="middle">
-                <Col span={2}>
-                    <div/>
-                </Col>
-                <Col span={22}>
-                    <span className="header2-text">Appointment Time : </span>
-                    <span className="header1-text">{start.format("HH:mm")}</span>
-                </Col>
-            </Row>
             <Row type="flex" align="middle" style={{paddingTop:10}}>
-                <Col span={2}>
-                    <div/>
-                </Col>
-                <Col span={10}>
-                    <Button variant="contained"  startIcon={<SaveIcon />}
-                             color="primary" onClick={() => saveButtonClicked(eventItem)}>
-                        Save
-                    </Button>
-                </Col>
-                <Col span={2}>
-                    <div/>
-                </Col>
-                <Col span={10}>
-                    <Button variant="outlined" color="secondary">
+                <Col span={10}/>
+                <Col span={3}>
+                    <Button color="#5D5D5D">
                         Cancel
                     </Button>
                 </Col>
+                <Col span={2}/>
+                <Col span={8}>
+                    <Button variant="contained"
+                            color="primary" onClick={() => saveButtonClicked(eventItem)}>
+                        Book Appointment
+                    </Button>
+                </Col>
+                <Col span={1}/>
             </Row>
         </div>);
 }
---
+++ client/src/utils/getAllStaffList.js
@@ -0,0 +1,298 @@
+export const allStaffList = [
+    {
+        "ID": 50841,
+        "Name": "Blisse Donner"
+    },
+    {
+        "ID": 50845,
+        "Name": "James Cessford"
+    },
+    {
+        "ID": 50862,
+        "Name": "Crissie Moxson"
+    },
+    {
+        "ID": 51136,
+        "Name": "Stevena Scargle"
+    },
+    {
+        "ID": 51138,
+        "Name": "Symon Newlands"
+    },
+    {
+        "ID": 51330,
+        "Name": "Frasco Boakes"
+    },
+    {
+        "ID": 51427,
+        "Name": "Aksel Acey"
+    },
+    {
+        "ID": 51458,
+        "Name": "Ethel MacRanald"
+    },
+    {
+        "ID": 51470,
+        "Name": "Gray Burr"
+    },
+    {
+        "ID": 51592,
+        "Name": "Millicent Hilliam"
+    },
+    {
+        "ID": 51594,
+        "Name": "Elliot Davitashvili"
+    },
+    {
+        "ID": 51606,
+        "Name": "Erina Helliker"
+    },
+    {
+        "ID": 160018,
+        "Name": "Blondell McClaurie"
+    },
+    {
+        "ID": 185981,
+        "Name": "Alyse Bramhall"
+    },
+    {
+        "ID": 201304,
+        "Name": "Chloette Marsh"
+    },
+    {
+        "ID": 219138,
+        "Name": "Andromache Hauch"
+    },
+    {
+        "ID": 240486,
+        "Name": "Sabra Crippill"
+    },
+    {
+        "ID": 274224,
+        "Name": "Lancelot Saffe"
+    },
+    {
+        "ID": 274275,
+        "Name": "Boycey Clamp"
+    },
+    {
+        "ID": 274752,
+        "Name": "Felicle Laweles"
+    },
+    {
+        "ID": 274753,
+        "Name": "Micki McTurlough"
+    },
+    {
+        "ID": 274754,
+        "Name": "Helenelizabeth Ahmed"
+    },
+    {
+        "ID": 275751,
+        "Name": "Kassi Riddeough"
+    },
+    {
+        "ID": 275948,
+        "Name": "Bernete Kinker"
+    },
+    {
+        "ID": 277215,
+        "Name": "Addy Cristofano"
+    },
+    {
+        "ID": 277219,
+        "Name": "Kalie Seiler"
+    },
+    {
+        "ID": 291448,
+        "Name": "Pepi Dockwray"
+    },
+    {
+        "ID": 291455,
+        "Name": "Patty Antonacci"
+    },
+    {
+        "ID": 404883,
+        "Name": "Doy Avent"
+    },
+    {
+        "ID": 512559,
+        "Name": "Sarina Eingerfield"
+    },
+    {
+        "ID": 521272,
+        "Name": "Cyrus Callway"
+    },
+    {
+        "ID": 523090,
+        "Name": "Keene Millea"
+    },
+    {
+        "ID": 532758,
+        "Name": "Rosa Quixley"
+    },
+    {
+        "ID": 544670,
+        "Name": "Talia Barthram"
+    },
+    {
+        "ID": 592720,
+        "Name": "Arman Janaszewski"
+    },
+    {
+        "ID": 610555,
+        "Name": "Lorine Frears"
+    },
+    {
+        "ID": 611804,
+        "Name": "Marcelia Kobieriecki"
+    },
+    {
+        "ID": 611809,
+        "Name": "Elliott Dust"
+    },
+    {
+        "ID": 653657,
+        "Name": "Cathrin Quant"
+    },
+    {
+        "ID": 653665,
+        "Name": "Kennan Bondar"
+    },
+    {
+        "ID": 654619,
+        "Name": "Megan Meader"
+    },
+    {
+        "ID": 660425,
+        "Name": "Latrena Corsan"
+    },
+    {
+        "ID": 698149,
+        "Name": "Salomone Hubery"
+    },
+    {
+        "ID": 699364,
+        "Name": "Jackson Mundall"
+    },
+    {
+        "ID": 733723,
+        "Name": "Margot Beteriss"
+    },
+    {
+        "ID": 786950,
+        "Name": "Julianne Muncie"
+    },
+    {
+        "ID": 791802,
+        "Name": "Kipp Glitherow"
+    },
+    {
+        "ID": 793332,
+        "Name": "Min Pym"
+    },
+    {
+        "ID": 793334,
+        "Name": "Amie Stocker"
+    },
+    {
+        "ID": 793342,
+        "Name": "Mara Coffee"
+    },
+    {
+        "ID": 796624,
+        "Name": "Camella Zecchi"
+    },
+    {
+        "ID": 796954,
+        "Name": "Ema McVity"
+    },
+    {
+        "ID": 797048,
+        "Name": "Artemus Ditt"
+    },
+    {
+        "ID": 831105,
+        "Name": "Mano Rimmington"
+    },
+    {
+        "ID": 831896,
+        "Name": "Cal Bubbear"
+    },
+    {
+        "ID": 831941,
+        "Name": "Fernande Kuzma"
+    },
+    {
+        "ID": 887406,
+        "Name": "Lamond Cayette"
+    },
+    {
+        "ID": 903702,
+        "Name": "Gigi Talmadge"
+    },
+    {
+        "ID": 904516,
+        "Name": "Sanson La Vigne"
+    },
+    {
+        "ID": 904520,
+        "Name": "Milzie Emmitt"
+    },
+    {
+        "ID": 904533,
+        "Name": "Royce Dunniom"
+    },
+    {
+        "ID": 914617,
+        "Name": "Jody Corkett"
+    },
+    {
+        "ID": 916038,
+        "Name": "Brina Etchells"
+    },
+    {
+        "ID": 917195,
+        "Name": "Quinton Stallebrass"
+    },
+    {
+        "ID": 925449,
+        "Name": "Alley Klausen"
+    },
+    {
+        "ID": 957791,
+        "Name": "Titus Lakey"
+    },
+    {
+        "ID": 957796,
+        "Name": "Tori Huc"
+    },
+    {
+        "ID": 957800,
+        "Name": "Portie Spendlove"
+    },
+    {
+        "ID": 961111,
+        "Name": "Leonerd Fagan"
+    },
+    {
+        "ID": 976588,
+        "Name": "Cy Bolsteridge"
+    },
+    {
+        "ID": 981812,
+        "Name": "Erda Cornbell"
+    },
+    {
+        "ID": 993767,
+        "Name": "Barret Moffett"
+    },
+    {
+        "ID": 997968,
+        "Name": "Deloris Vickar"
+    },
+    {
+        "ID": 1007205,
+        "Name": "Garrett Fedder"
+    }
+];
