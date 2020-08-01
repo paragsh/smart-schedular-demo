93b5aaaac410470f0e695317a49f640aefa7ea1a
Anil Indorkar <Anil.Indorkar@mindbodyonline.com>
2020-07-31 22:35:49 -0700

    css changes by ganesh

--- client/src/component/CustomHeader/CustomHeader.js
+++ client/src/component/CustomHeader/CustomHeader.js
@@ -4,12 +4,13 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from '@material-ui/core/Typography';
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 
 const CustomHeader = () => {
     return(
         <AppBar position="static">
             <Toolbar>
-                <div class={"logo"}></div>
+                <AddToPhotosIcon class={"logo"} />
                 <Typography class={"brand"}>
                     <div class={"BrandName"}>Smarter</div>
                     <div class={"StudioName"}>Chenz Salon and Spa, Pune (IN)</div>
